667f3566dfd69e818f4f23b2aa8bf06cab4f25bc
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2025-06-13 23:23:18 +0800

    Update P-6-7. LCS.cpp

--- P-6-7. LCS.cpp
+++ P-6-7. LCS.cpp
@@ -8,24 +8,16 @@ int main(){
     string A,B;
     cin>>A>>B;
     int lenA=A.size(),lenB=B.size();
-    //cout<<lenA<<" "<<lenB<<endl;
-    vector<vector<int>> LCS(lenA+1,vector<int>(lenB+1,0));
-    //cout<<LCS.size()<<' '<<LCS[0].size()<<endl;
-    for (int i=1;i<=lenA;++i){
+    vector<int> pLCS(lenB+1),cLCS(lenB+1);
+
+    for (int i=0;i<lenA;++i){
         for (int j=1;j<=lenB;++j){
-            LCS[i][j]=max(LCS[i-1][j],LCS[i][j-1]);
-            if (A[i-1]==B[j-1]) LCS[i][j]=max(LCS[i][j],LCS[i-1][j-1]+1);
+            cLCS[j]=max(pLCS[j],cLCS[j-1]);
+            if (A[i]==B[j-1]) cLCS[j]=max(cLCS[j],pLCS[j-1]+1);
         }
+        swap(pLCS,cLCS);
     }
-    /*
-    for (int i=0;i<=lenA;++i){
-        for (int j=0;j<=lenB;++j){
-            cout<<LCS[i][j]<<' ';
-        }
-        cout<<endl;
-    }
-    */
-    cout<<LCS[lenA][lenB];
+    cout<<cLCS.back();
 
     return 0;
 }
