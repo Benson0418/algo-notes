fd9258a15609ef02552f28fa8734d375e84bc799
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2025-06-14 01:58:58 +0800

    Add files via upload

--- P-6-9. 大賣場免費大搬家.cpp
+++ P-6-9. 大賣場免費大搬家.cpp
@@ -4,26 +4,22 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n,W;
-    cin>>n>>W;
-    vector<int> T(n);
-    vector<pair<int,int>> M(n); //W T
-    vector<int> pdp(W+1),cdp(W+1);
-    for(int& x:T) cin>>x;
+
+    int n,C;
+    cin>>n>>C;
+    vector<int> W(n),V(n),dp(C+1);
+
+    for(int& x:W) cin>>x;
+    for(int& x:V) cin>>x;
+
     for (int i=0;i<n;++i){
-        M[i].first=T[i];
-        cin>>M[i].second;
-    }
-    for (int i=0;i<n;++i){
-        for (int j=1;j<=W;++j){
-            cdp[j]=pdp[j];
-            if (j>=M[i].first){
-                cdp[j]=max(cdp[j],pdp[j-M[i].first]+M[i].second);
+        for (int j=C;j>=0;--j){
+            if (j>=W[i]){
+                dp[j]=max(dp[j],dp[j-W[i]]+V[i]);
             }
         }
-        swap(cdp&,pdp&);
     }
-    cout<<pdp[W];
+    cout<<dp[C];
 
     return 0;
 }
