da9c20e29b78c2d8f9b9f23e02c20f7bcfa30156
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2025-08-09 14:09:04 +0800

    Update 2477. 到达首都的最少油耗.cpp

--- leetcode/2477. 到达首都的最少油耗.cpp
+++ leetcode/2477. 到达首都的最少油耗.cpp
@@ -14,6 +14,42 @@ public:
             to[i++]=u;
         }
         
+        auto f=[&](auto&& self,int idx,int pa) ->pair<long long,long long>{
+            long long res=0,ppl=0;
+            for (int k=head[idx];k!=-1;k=edges[k]){
+                if (to[k]==pa) continue;
+                pair<long long,long long> e=self(self,to[k],idx);
+                res+=e.first;
+                ppl+=e.second;
+            }
+            ++ppl;
+            res+=(ppl-1)/seats+1;
+
+            if (idx==0) return make_pair(res-((ppl-1)/seats+1),ppl);
+            return make_pair(res,ppl);
+        };
+
+        return  f(f,0,-1).first;
+    }
+};
+
+/*
+class Solution {
+public:
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n=roads.size()+1;
+        vector<int> head(n,-1),edges(2*n-2,-1),to(2*n-2,-1);
+        int i=0;
+        for (auto& ed : roads){
+            int u=ed[0],v=ed[1];
+            edges[i]=head[u];
+            head[u]=i;
+            to[i++]=v;
+            edges[i]=head[v];
+            head[v]=i;
+            to[i++]=u;
+        }
+        
         function<pair<long long,long long>(int,int)> f=[&](int idx,int pa){
             long long res=0,ppl=0;
             for (int k=head[idx];k!=-1;k=edges[k]){
@@ -32,3 +68,4 @@ public:
         return  f(0,-1).first;
     }
 };
+*/
