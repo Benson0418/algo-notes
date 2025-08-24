d0fd222875f6f54252ba0411767fddce4bb30baf
Benson0418 <97838905+Benson0418@users.noreply.github.com>
2025-08-24 23:08:49 +0800

    Create 習題 Q-2-8. 模逆元(exgcd).cpp

---
+++ 習題 Q-2-8. 模逆元(exgcd).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,P;
+    cin>>n>>P;
+
+    int x,y,d;
+    auto exgcd=[&](auto&& self,int a,int b) ->int{
+        if (b==0){
+            d=a;
+            x=1;
+            y=0;
+            return a;
+        }
+        d=self(self,b,a%b);
+        tie(x,y)=make_pair(y,x-(a/b)*y);
+        return d;
+    };
+    auto inv=[&](int a,int m) ->int{
+        return (exgcd(exgcd,a,m)==1?(x%m+m)%m:-1);
+    };
+    while (n--){
+        int num;
+        cin>>num;
+        cout<< inv(num,P) << " ";
+    }
+
+
+    return 0;
+}
